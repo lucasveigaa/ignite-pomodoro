8660d10243070131f67d31991873f036e8e280eb
Lucas Veiga <lucasveigagonzaga@yahoo.com.br>
2022-07-21 10:47:53 -0300

    Rearranging folders, creating a contexts folder to organize better the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
-import { BrowserRouter } from 'react-router-dom'
-import { Router } from './Router'
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
+import { CyclesContextProvider } from "./contexts/CyclesContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-       <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
